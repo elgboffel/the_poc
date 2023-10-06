dffb0cd4bf0ad2a4b6581ecfb6089419ecdb5aea
jok <jok@kruso.dk>
2023-10-06 22:06:37 +0200

    test

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,34 +1,13 @@
-use axum::{routing::{get, post}, http::StatusCode, Json, Router, ServiceExt};
-use serde::{Deserialize, Serialize};
+use axum::{routing::{get}, Router, ServiceExt};
 use std::net::SocketAddr;
-use axum::extract::State;
-use star_wars_sdk::AllFilmsQuery;
-use dotenvy::dotenv;
-// use database::DbConnection;
-use entity::post;
-
-
-// #[derive(Clone)]
-// struct AppState {
-//     db: DbConnection
-// }
-
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
 
     tracing_subscriber::fmt::init();
 
-    // let state = AppState {
-    //     db: database::connect_db().await
-    // };
-
     let app = Router::new()
-        .route("/", get(root))
-        .route("/all-films", get(all_films))
-        // .route("/all-posts", get(all_posts))
-        .route("/users", post(create_user));
+        .route("/", get(root));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
@@ -43,43 +22,4 @@ async fn main() {
 
 async fn root() -> String {
     format!("API v{}", 1)
-}
-
-// #[axum_macros::debug_handler]
-// async fn all_posts(
-//     State(state): State<AppState>,
-// ) -> (StatusCode, Json<Vec<post::Model>>) {
-//
-//     let response = database::get_all_posts(&state.db).await;
-//
-//     (StatusCode::OK, Json(response))
-// }
-
-async fn all_films(
-) -> (StatusCode, Json<AllFilmsQuery>) {
-    let response = star_wars_sdk::get_all_films().await;
-
-    (StatusCode::OK, Json(response.data.unwrap()))
-}
-
-async fn create_user(
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    (StatusCode::CREATED, Json(user))
-}
-
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
